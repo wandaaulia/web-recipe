b31673cc06df2aacfed2ef2e7b3fc8fa13b26686
wanda <wandaul16@gmail.com>
2022-08-08 22:30:27 +0700

    update

--- src/Routers.jsx
+++ src/Routers.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import DetailFood from "./pages/DetailFood";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ProtectedRoute from "./component/ProtectedRoute";
