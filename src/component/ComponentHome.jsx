0b7fefd0fe7dd34f5dbfe0218364451835566a0c
wanda <wandaul16@gmail.com>
2022-08-13 13:57:26 +0700

    fix fonts

--- src/component/ComponentHome.jsx
+++ src/component/ComponentHome.jsx
@@ -4,8 +4,8 @@ import JapanFood from './JapanFood'
 
 const ComponentHome = () => {
   return (
-     <div className='mt-8 md:mt-8 xl:mt-14 mx-auto flex flex-col items-center w-11/12 '> 
-        <h1 className='text-lg w-full font-semibold mb-1 xl:text-2xl  2xl:text-3xl'> 
+     <div className='mt-8 md:mt-10 xl:mt-16 lg:mt-14 mx-auto flex flex-col items-center w-11/12 '> 
+        <h1 className='text-lg w-full font-semibold mb-2 xl:text-2xl  md:mb-4 lg:mb-5 xl:mb-8 2xl:text-3xl'> 
       Chocolate Recipes from Around the World
          </h1> 
 
