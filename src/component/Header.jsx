dceab75b02ab70f628e3c3eeb7df0e5abac44934
wanda <wandaul16@gmail.com>
2022-08-14 17:06:04 +0700

    change header

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -3,12 +3,13 @@ import { MdSearch } from "react-icons/md";
 import { useNavigate } from "react-router-dom";
 import {  useDispatch } from 'react-redux'
 import { unSetActiveCat } from '../features/recipeSlice';
+import bannerimg from '../assets/img/banner-img-header.png';
+
 
 const Header = () => {
     const [search, setSearch] = useState('');
 
-    const navigate = useNavigate();
-
+   const navigate = useNavigate();
         
    const dispatch = useDispatch();
 
@@ -27,8 +28,29 @@ const Header = () => {
 
 
   return (
-    <> 
-    <div className='background-header-recipe flex flex-col  w-full mx-auto pt-8 lg:pt-4 items-center justify-center'
+    <div className='w-full height-banner relative '> 
+  <div className="z-20 w-full h-full backdrop-brightness-50 bg-cover">
+            <video poster={bannerimg} width="400" autoPlay muted loop className='z-10 w-full h-full object-cover object-center backdrop-brightness-50'>
+                <source src="https://firebasestorage.googleapis.com/v0/b/food-app-6de2d.appspot.com/o/slice_cooking.mp4?alt=media&token=f3ed095f-be89-437e-af4d-176c4d74fef9" type="video/mp4"/>
+                Your Browser does not support HTML video.
+            </video>
+    </div>
+     <div className='overlay z-30 absolute top-0 left-0 flex flex-col h-full w-full justify-center items-center'
+    > 
+    <div> 
+      <h1 className=' text-center font-bold text-white text-3xl md:text-4xl mb-4 md:pb-2 lg:pb-4 lg:text-5xl'> WE RECIPE </h1>
+          <p className='text-center font-medium px-8 text-white' > Helping you cook a variety of dishes from all over the world  </p>
+   </div>
+   <div className='hidden md:flex pt-8'>
+      <form id="form" className='bg-white w-full md:py-2 lg:py-0 focus:border focus:border-solid focus:border-red-500 md:px-8 rounded' onSubmit={handleSubmit}> 
+     <input type="text" id="name" name="name" placeholder='Search recipes by name' value={search} onChange={(e) => setSearch(e.target.value)} className='lg:py-4  w-96 py-1 text-lg placeholder:text-gray-400'/>
+       <button type="submit" className='text-lg text-gray-400'> 
+     <MdSearch />
+        </button>
+      </form>
+   </div>
+    </div>
+    {/* <div className='background-header-recipe flex flex-col  w-full mx-auto pt-8 lg:pt-4 items-center justify-center'
     > 
     <div> 
       <h1 className=' text-center font-bold color-title-header text-3xl pb-3 md:text-4xl md:pb-2 lg:pb-4 lg:text-5xl'> WE RECIPE </h1>
@@ -42,8 +64,9 @@ const Header = () => {
         </button>
       </form>
    </div>
+    </div> */}
+    
     </div>
-    </>
   )
 }
 
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-
+import { getStorage} from 'firebase/storage';
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_KEY,
@@ -16,6 +16,8 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+export const storage = getStorage(app);
+
 export const provider = new GoogleAuthProvider();
 
 export { auth };
