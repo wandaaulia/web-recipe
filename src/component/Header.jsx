ec7659512516db38a951e7c12adcc6a03b882a52
wanda <wandaul16@gmail.com>
2022-08-14 19:48:00 +0700

    fix banner

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -3,13 +3,12 @@ import { MdSearch } from "react-icons/md";
 import { useNavigate } from "react-router-dom";
 import {  useDispatch } from 'react-redux'
 import { unSetActiveCat } from '../features/recipeSlice';
-import bannerimg from '../assets/img/banner-img-header.png';
-
 
 const Header = () => {
     const [search, setSearch] = useState('');
 
-   const navigate = useNavigate();
+    const navigate = useNavigate();
+
         
    const dispatch = useDispatch();
 
@@ -28,7 +27,24 @@ const Header = () => {
 
 
   return (
-    <div className='w-full height-banner relative '> 
+    <> 
+    <div className='background-header-recipe flex flex-col  w-full mx-auto pt-8 lg:pt-4 items-center justify-center'
+    > 
+    <div> 
+      <h1 className=' text-center font-bold color-title-header text-3xl pb-3 md:text-4xl md:pb-2 lg:pb-4 lg:text-5xl'> WE RECIPE </h1>
+          <p className='text-center px-8 text-color-header ' > Helping you cook a variety of dishes from all over the world  </p>
+   </div>
+   <div className='hidden md:flex pt-8'>
+      <form id="form" className='bg-white w-full md:py-2 lg:py-0 focus:border focus:border-solid focus:border-red-500 md:px-8 rounded' onSubmit={handleSubmit}> 
+     <input type="text" id="name" name="name" placeholder='Search recipes by name' value={search} onChange={(e) => setSearch(e.target.value)} className='lg:py-4  w-96 py-1 text-lg placeholder:text-gray-400'/>
+       <button type="submit" className='text-lg text-gray-400'> 
+     <MdSearch />
+        </button>
+      </form>
+   </div>
+    </div>
+
+         {/* <div className='w-full height-banner relative '> 
   <div className="z-20 w-full h-full backdrop-brightness-50 bg-cover">
             <video poster={bannerimg} width="400" autoPlay muted loop className='z-10 w-full h-full object-cover object-center backdrop-brightness-50'>
                 <source src="https://firebasestorage.googleapis.com/v0/b/food-app-6de2d.appspot.com/o/slice_cooking.mp4?alt=media&token=f3ed095f-be89-437e-af4d-176c4d74fef9" type="video/mp4"/>
@@ -50,23 +66,10 @@ const Header = () => {
       </form>
    </div>
     </div>
-    {/* <div className='background-header-recipe flex flex-col  w-full mx-auto pt-8 lg:pt-4 items-center justify-center'
-    > 
-    <div> 
-      <h1 className=' text-center font-bold color-title-header text-3xl pb-3 md:text-4xl md:pb-2 lg:pb-4 lg:text-5xl'> WE RECIPE </h1>
-          <p className='text-center px-8 text-color-header ' > Helping you cook a variety of dishes from all over the world  </p>
-   </div>
-   <div className='hidden md:flex pt-8'>
-      <form id="form" className='bg-white w-full md:py-2 lg:py-0 focus:border focus:border-solid focus:border-red-500 md:px-8 rounded' onSubmit={handleSubmit}> 
-     <input type="text" id="name" name="name" placeholder='Search recipes by name' value={search} onChange={(e) => setSearch(e.target.value)} className='lg:py-4  w-96 py-1 text-lg placeholder:text-gray-400'/>
-       <button type="submit" className='text-lg text-gray-400'> 
-     <MdSearch />
-        </button>
-      </form>
-   </div>
-    </div> */}
+ 
     
-    </div>
+    </div> */}
+    </>
   )
 }
 
